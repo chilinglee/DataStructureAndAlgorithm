654b54492d9f2c37aee4c2820ee58e2c0c6bb7cb
Jill Lee <jill@twjoin.com>
2024-10-05 14:19:02 +0800

    feat: fibonacci recursion

---
+++ DataStructureAndAlgorithm/Services/FibonacciService.cs
@@ -0,0 +1,24 @@
+﻿namespace Services;
+
+public class FibonacciService
+{
+    /// <summary>
+    /// 費波那切數列
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns></returns>
+    public int Fibonacci(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+
+        if(n == 1)
+        {
+            return 1;
+        }
+
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 記錄資料結構與演算法學習筆記，程式語言為C#
 
 ## 資料結構入門與演算法
+1. **遞回法 Recursion** 
+如果一個函式由自身所定義與調用，則是遞回法 Recursion。使用 Recursion 的函式需具備以下兩個條件：
+- 可以重複調用自身函式
+- 跳出執行的條件出口
+
+案例：[費波那切數列](./DataStructureAndAlgorithm/Services/FibonacciService.cs)
